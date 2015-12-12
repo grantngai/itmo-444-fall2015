b62231fd93c5737623b67a14794342ede50e5586
grantngai <pngai@hawk.iit.edu>
2015-12-11 23:38:00 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head><title>Hello World</title>
+</head>
+<body>
+
+<form enctype="multipart/form-data" action="result.php" method="POST">
+
+<input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+
+Send this file: <input name="userfile" type="file" /><br />
+Enter Email of user: <input name="userfile" type="file" /><br />
+Enter Phone of user (1xxxxxxxxxx): <input type="phone" name="phone">
+
+<input type="submit" value="Send File" />
+</form>
+<hr />
+
+<form enctype="multipart/form-data" action="gallery.php" method="POST">
+
+Enter Email of user for gallery to browse: <input type="email" name="email">
+<input type="submit" value="Load Gallery" />
+</form>
+
+</body>
+</html>  
