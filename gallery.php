a189c4151ac3c844e57d80159793738c025ae5b1
grantngai <pngai@hawk.iit.edu>
2015-12-12 00:04:06 -0600

    added gallery.php

---
+++ gallery.php
@@ -0,0 +1,50 @@
+<html>
+<head><title>Gallery</title>
+</head>
+<body>
+
+<?php
+session_start();
+$email = $_POST["email"];
+echo $email;
+require 'vendor/autoload.php';
+
+use Aws\Rds\RdsClient;
+$client = RdsClient::factory(array(
+'version'=>'latest',
+'region'  => 'us-east-1'
+));
+
+$result = $client->describeDBInstances(array(
+    'DBInstanceIdentifier' => 'pngai-db',
+));
+
+$endpoint = "";
+
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+    // Do something with the message
+    echo "============". $ep . "================";
+
+  
+//echo "begin database";
+$link = mysqli_connect($endpoint,"controller","pngai-db","itmo444db") or die("Error " . mysqli_error($link));
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+//below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
+$link->real_query("SELECT * FROM items WHERE email = '$email'");
+//$link->real_query("SELECT * FROM items");
+$res = $link->use_result();
+echo "Result set order...\n";
+while ($row = $res->fetch_assoc()) {
+    echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
+echo $row['id'] . "Email: " . $row['email'];
+}
+$link->close();
+?>
+</body>
+</html>
